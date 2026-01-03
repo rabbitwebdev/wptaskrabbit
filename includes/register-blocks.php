115fb040a9e5e896ec3c5990f845c80cb79888c2
pythedev <pip@rabbitwebdesign.co.uk>
2026-01-03 23:57:41 +0000

    v2.7

--- includes/register-blocks.php
+++ includes/register-blocks.php
@@ -8,37 +8,38 @@ if (!defined('ABSPATH')) {
 //     register_block_type(__DIR__ . '/blocks/task-form');
 // });
 
-add_action('init', function () {
+function task_register_custom_block() {
    
 
     wp_register_script(
         'wprabbit-task-form-editor-script',
-        plugin_dir_url(__FILE__) . '/includes/blocks/task-form/index.js',
+        plugin_dir_url(__FILE__) . '/blocks/task-form/index.js',
         array('wp-blocks', 'wp-editor'),
-        filemtime(plugin_dir_path(__FILE__) . '/includes/blocks/task-form/index.js'),
+        filemtime(plugin_dir_path(__FILE__) . '/blocks/task-form/index.js'),
         true
     );
 
     wp_register_style(
         'wprabbit-task-form-style',
-        plugin_dir_url(__FILE__) . '/includes/blocks/task-form/style.css',
+        plugin_dir_url(__FILE__) . '/blocks/task-form/style.css',
         array(),
-        filemtime(plugin_dir_path(__FILE__) . '/includes/blocks/task-form/style.css')
+        filemtime(plugin_dir_path(__FILE__) . '/blocks/task-form/style.css')
     );
 
      wp_register_script(
         'task-block-editor',
-        plugin_dir_url(__FILE__) . '/includes/blocks/task-form/editor.js',
+        plugin_dir_url(__FILE__) . '/blocks/task-form/editor.js',
         [ 'wp-blocks', 'wp-element', 'wp-editor', 'wp-components', 'wp-i18n' ],
-        filemtime( plugin_dir_path( __FILE__ ) . '/includes/blocks/task-form/editor.js' ),
+        filemtime( plugin_dir_path( __FILE__ ) . '/blocks/task-form/editor.js' ),
         true
     );
 
-     register_block_type( plugin_dir_path( __FILE__ ) . '/includes/blocks/task-form/', [
+     register_block_type( plugin_dir_path( __FILE__ ) . '/blocks/task-form/', [
         'editor_script' => 'task-block-editor',
          'render_callback' => 'wp_render_task_block',
     ]);
-});
+}
+add_action('init', 'task_register_custom_block');
 
 
 function wp_render_task_block( $attributes ) {
